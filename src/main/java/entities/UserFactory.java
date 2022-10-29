a1cf2b5f59a0c91739d44f3e3182ce4d03ed327b
Eric <eric.h@signority.com>
2022-10-29 10:29:12 -0400

    Added public to UserFactory

--- src/main/java/entities/UserFactory.java
+++ src/main/java/entities/UserFactory.java
@@ -3,7 +3,7 @@ package entities;
 import java.util.List;
 
 public class UserFactory {
-    User create(String username, String password, String email, List<Integer> chats, List<Integer> feeds) {
+    public User create(String username, String password, String email, List<Integer> chats, List<Integer> feeds) {
         return new User(username, password, email, chats, feeds);
     };
 }
