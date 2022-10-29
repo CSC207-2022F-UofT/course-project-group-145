7490d799b73a8f24a8b613a2e7f08e1ab30d7224
Eric <eric.h@signority.com>
2022-10-29 12:31:56 -0400

    Removed unnecessary semicolon

--- src/main/java/entities/UserFactory.java
+++ src/main/java/entities/UserFactory.java
@@ -5,5 +5,5 @@ import java.util.List;
 public class UserFactory {
     public User create(String username, String password, String email, List<Integer> chats, List<Integer> feeds) {
         return new User(username, password, email, chats, feeds);
-    };
+    }
 }
