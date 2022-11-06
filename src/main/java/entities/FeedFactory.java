1f72454fd29ae6f37121a0ce38f91330b054187e
kermitpotatoslide <aaronw1091@gmail.com>
2022-11-06 15:17:29 -0500

    Added Feed and Tag entities, as well as FeedFactory

---
+++ src/main/java/entities/FeedFactory.java
@@ -0,0 +1,12 @@
+package entities;
+
+import java.util.List;
+
+public class FeedFactory {
+
+    public Feed generateNewFeed(List<String> tags){
+        // TODO: things
+        return null;
+    }
+
+}
