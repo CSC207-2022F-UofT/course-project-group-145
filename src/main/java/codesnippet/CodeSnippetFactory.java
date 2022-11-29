b78513a8441f29803542cf69625c43967dcbd583
Nik Danilov <nik.danilov@mail.utoronto.ca>
2022-11-28 19:39:34 -0500

    fix: fix

--- src/main/java/codesnippet/CodeSnippetFactory.java
+++ src/main/java/codesnippet/CodeSnippetFactory.java
@@ -4,7 +4,6 @@ import entities.CodeSnippet;
 
 public class CodeSnippetFactory {
     public CodeSnippet create(int id, String title, int userId, String fileUrl) {
-
         return new CodeSnippet(id, userId, title, fileUrl);
     }
 }
