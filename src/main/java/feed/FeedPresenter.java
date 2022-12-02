f719b0d16a67cfb0098b6e403c7a3201afa115f1
kermitpotatoslide <aaronw1091@gmail.com>
2022-12-02 18:34:54 -0500

    Update FeedPresenter.java

--- src/main/java/feed/FeedPresenter.java
+++ src/main/java/feed/FeedPresenter.java
@@ -3,7 +3,13 @@ package feed;
 public class FeedPresenter implements CreateFeedOutputBoundary{
 
     private FeedViewModel feedViewModel;
+    private FeedDSRepository repo;
+    //TODO: User repo reference
 
+    /**
+     * successView called by CreateFeedUseCase to signify that use case was successful.
+     * In this case, a new feed was created, and that the feedViewModel should be updated with a new list of feeds
+     */
     @Override
     public void successView() {
 
