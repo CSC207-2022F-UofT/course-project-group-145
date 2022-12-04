6c0dd134ff967a3126e66fbe234aa9e048fb2238
Rubin Rastogi <rubin.rastogi@mail.utoronto.ca>
2022-12-03 20:18:32 -0500

    - Small update, added openListChat to the ChatDeletionOutputBoundary interface

--- src/main/java/controller_presenter_gateway/chat_list_controller_presenter_gateway/ChatDeletionOutputBoundary.java
+++ src/main/java/controller_presenter_gateway/chat_list_controller_presenter_gateway/ChatDeletionOutputBoundary.java
@@ -3,5 +3,7 @@ package controller_presenter_gateway.chat_list_controller_presenter_gateway;
 public interface ChatDeletionOutputBoundary {
     void successView(ChatDeletionResponseModel responseModel);
 
+    void openListChat(int userId);
+
     void failView(String error);
 }
